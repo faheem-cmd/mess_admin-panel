d4b3ac864dcd0a6a94bcc23cd34675cc12c0d4be
Muhammed <faheem@metrictreelabs.com>
2022-08-01 10:36:18 +0530

    fwf

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
   const navigate = useNavigate();
-
   const handleNavigate = () => {
     navigate("/sign-up");
   };
+
+  const notify = () =>
+    toast.success("Successfully logged!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
 
   return (
     <form>
@@ -37,23 +49,34 @@ const Login = () => {
             className="custom-control-input"
             id="customCheck1"
           />
-          <label className="custom-control-label" htmlFor="customCheck1">
+          <label
+            className="custom-control-label"
+            htmlFor="customCheck1"
+            style={{ paddingLeft: 10 }}
+          >
             Remember me
           </label>
         </div>
       </div>
 
       <div className="d-grid">
-        <button
-          type="submit"
-          className="btn btn-primary"
-          onClick={handleNavigate}
-        >
+        <button type="submit" className="btn btn-primary" onClick={notify}>
           Submit
         </button>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </div>
-      <p className="forgot-password text-right">
-        Forgot <a href="#">password?</a>
+      <p className="forgot-password text-right" onClick={handleNavigate}>
+        Not <a href="#">Register ?</a>
       </p>
     </form>
   );
